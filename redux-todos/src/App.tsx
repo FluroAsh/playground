5b9f5529fabe9829289cc6de1d6c3d4c2bff8f1b
Ashley Thompson <workashleythompson@gmail.com>
2023-03-22 23:35:54 +1100

    feature: adds json-server & basic form structure

--- redux-todos/src/App.tsx
+++ redux-todos/src/App.tsx
@@ -1,8 +1,21 @@
+import { FormEvent, MouseEvent } from 'react'
+
 function App() {
+  const handleClick = (event: MouseEvent<HTMLButtonElement>) => {
+    // Do something
+  }
+
+  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault()
+    // Make a POST request...
+    console.log('pretend I made a POST request')
+  }
   return (
     <div className="App">
-      {/* Thing to stop the block collapsing */}
-      {/* Stuff go here  🐒 */}
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="title">Todo Title</label>
+        <input name="title" type="text" />
+      </form>
     </div>
   )
 }
