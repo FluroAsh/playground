89b576fa47133ddd68df858218aa8a449d7b3c81
Ashley Thompson <workashleythompson@gmail.com>
2023-03-27 12:57:30 +1100

    feat: adds additional types for Redux store

--- redux-todos/src/app/store.ts
+++ redux-todos/src/app/store.ts
@@ -1,5 +1,34 @@
 import { configureStore } from '@reduxjs/toolkit'
+import { useDispatch, useSelector, TypedUseSelectorHook } from 'react-redux'
 
-export default configureStore({
-  reducer: {}
+import todoReducer from '../slices/todos'
+
+const preloadedState = {
+  todos: [
+    {
+      id: 1,
+      todo: 'Learn React',
+      description: 'Learn React and Redux',
+      completed: false
+    },
+    {
+      id: 2,
+      todo: 'Learn Redux',
+      description: 'Learn Redux and React-Redux',
+      completed: false
+    }
+  ]
+}
+
+const store = configureStore({
+  reducer: todoReducer,
+  devTools: true,
+  preloadedState
 })
+
+export type RootState = ReturnType<typeof store.getState>
+export type AppDispatch = typeof store.dispatch
+export const useAppDispatch = () => useDispatch<AppDispatch>()
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector
+
+export default store
