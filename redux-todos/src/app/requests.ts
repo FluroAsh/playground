c4939ee25ed496b373186e3362e50676e5d0a141
Ashley Thompson <workashleythompson@gmail.com>
2023-03-27 12:59:57 +1100

    refactor: removes redundant body param for getRequest

--- redux-todos/src/app/requests.ts
+++ redux-todos/src/app/requests.ts
@@ -1,12 +1,11 @@
 const baseUrl = 'http://localhost:3000/'
 
-export const getRequest = (body: any, endpoint: string) => {
+export const getRequest = (endpoint: string) => {
   return fetch(baseUrl + endpoint, {
     method: 'GET',
     headers: {
       'Content-Type': 'application/json'
-    },
-    body: JSON.stringify(body)
+    }
   })
 }
 
