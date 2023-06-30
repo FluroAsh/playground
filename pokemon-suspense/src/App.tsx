a150f185ba75d3c2704d31f7e9e24916a781d108
Ashley Thompson <workashleythompson@gmail.com>
2023-07-01 01:53:15 +1000

    adds some fancy framer motion animations :o

--- pokemon-suspense/src/App.tsx
+++ pokemon-suspense/src/App.tsx
@@ -2,7 +2,8 @@ import { Suspense, lazy, useState, useEffect } from 'react'
 import './App.css'
 import { fetchPokemon } from './utils'
 
-const Pokemon = lazy(() => import('./components/Pokemon'))
+// const Pokemon = lazy(() => import('./components/Pokemon'))
+import Pokemon from './components/Pokemon'
 
 export const Skeleton = () => (
   <div className="skele-container">
@@ -13,37 +14,43 @@ export const Skeleton = () => (
 )
 
 function App() {
-  const [dittoData, setDittoData] = useState<any>(null)
-  const [pikachuData, setPikachuData] = useState<any>(null)
+  // const [dittoData, setDittoData] = useState<any>(null)
+  // const [pikachuData, setPikachuData] = useState<any>(null)
 
-  useEffect(() => {
-    const fetchDitto = () =>
-      setTimeout(async () => fetchPokemon('ditto', setDittoData), 1000)
-    fetchDitto()
-  }, [])
+  // TODO: Data fetching should happen inside an individual component
+  // This way we can track the data fetching state and handle it in the
+  // suspense boundary.
+  // useEffect(() => {
+  //   const fetchDitto = () =>
+  //     setTimeout(async () => fetchPokemon('ditto', setDittoData), 1000)
+  //   fetchDitto()
+  // }, [])
 
-  useEffect(() => {
-    const fetchPikachu = () =>
-      setTimeout(async () => fetchPokemon('pikachu', setPikachuData), 2000)
-    fetchPikachu()
-  }, [])
+  // useEffect(() => {
+  //   const fetchPikachu = () =>
+  //     setTimeout(async () => fetchPokemon('pikachu', setPikachuData), 2000)
+  //   fetchPikachu()
+  // }, [])
 
   console.log('loading...')
   return (
     <div className="App" style={{ display: 'flex', flexDirection: 'row' }}>
+      {/* TODO: Render the components at the same time regardless of when their data fetching completes */}
+      {/* <Suspense fallback={<div>Loading...</div>}> */}
       <div className="pokemon-container">
         <h1>Ditto</h1>
         <Suspense fallback={<Skeleton />}>
-          <Pokemon data={dittoData} />
+          <Pokemon name="ditto" />
         </Suspense>
       </div>
 
       <div className="pokemon-container">
         <h1>Pikachu</h1>
         <Suspense fallback={<Skeleton />}>
-          <Pokemon data={pikachuData} />
+          <Pokemon name="pikachu" />
         </Suspense>
       </div>
+      {/* </Suspense> */}
     </div>
   )
 }
--- pokemon-suspense/src/components/Pokemon.tsx
+++ pokemon-suspense/src/components/Pokemon.tsx
@@ -1,21 +1,43 @@
-import { Skeleton } from '../App'
+import { motion } from 'framer-motion'
+import useSWR from 'swr'
 
-const Pokemon: React.FC<{ data: any }> = ({ data }) => {
-  // Why do we need this if suspense should be rendering the
-  // falback UI when we have no data?
+const Pokemon: React.FC<{ name: string }> = ({ name }) => {
+  const { data, error } = useSWR(`https://pokeapi.co/api/v2/pokemon/${name}`, {
+    suspense: true
+  })
 
-  if (!data) return <Skeleton />
+  if (error) return <pre>{error.message}</pre>
 
-  const { id, name, sprites } = data
+  const variants = {
+    hidden: { opacity: 0 },
+    visible: { opacity: 1 }
+  }
+
+  const transition = {
+    duration: 0.5,
+    ease: 'easeInOut'
+  }
+
   const capitalize = (word: string) => word[0].toUpperCase() + word.slice(1)
 
   return (
     <>
-      <div style={{ marginBottom: '-20px' }}>
-        <p>{capitalize(name)}!</p>
-        <p>Pokedex #: {id}</p>
-        <img src={sprites['front_default']} height={150} />
-      </div>
+      <motion.div
+        variants={variants}
+        transition={transition}
+        initial="hidden"
+        animate="visible"
+        style={{ marginBottom: '-20px' }}
+      >
+        <p>{capitalize(data.name)}!</p>
+        <p>Pokedex #: {data.id}</p>
+        <motion.img
+          variants={variants}
+          transition={transition}
+          src={data.sprites['front_default']}
+          height={150}
+        />
+      </motion.div>
     </>
   )
 }
